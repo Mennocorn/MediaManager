24aec53c9e5db5fc1c8a06d0d244d8e0a36ad32e
Mennocorn <kilian.wiendl@gmx.de>
2022-03-30 17:47:06 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MediaManger',
+    version='0.0.1',
+    packages=['MediaManager'],
+    url='https://github.com/Mennocorn/MediaManger',
+    license='MIT',
+    author='Unicorn',
+    author_email='kilian.wiendl@gmx.de',
+    description='A simple tool to manage different media.'
+)
